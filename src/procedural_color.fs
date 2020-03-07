f02b7f24a6f1bd835260ef9de551bd8cd8f2080b
Yegor <yegor.zadniprovskyy@mail.utoronto.ca>
2020-03-06 19:43:58 -0500

    perlin works

--- src/procedural_color.fs
+++ src/procedural_color.fs
@@ -17,8 +17,16 @@ out vec3 color;
 // expects: blinn_phong, perlin_noise
 void main()
 {
-  /////////////////////////////////////////////////////////////////////////////
-  // Replace with your code 
-  color = vec3(1,1,1);
-  /////////////////////////////////////////////////////////////////////////////
+    vec3 ka = vec3(1,1,1);
+    vec3 l = normalize(vec3( cos(M_PI*animation_seconds/3), 2, sin(M_PI*animation_seconds/3)));
+
+    vec3 kd = vec3(0,0,1), ks = vec3(0.5,0.5,0.5);
+
+
+    if (is_moon) {
+        kd = vec3(0.5,0.5,0.5), ks = vec3(0.5,0.5,0.5);
+    }
+
+    color = kd * (perlin_noise((sphere_fs_in+1)*10)+1)/2;
+
 }
--- src/random_direction.glsl
+++ src/random_direction.glsl
@@ -8,8 +8,13 @@
 // expects: random2.glsl, PI.glsl
 vec3 random_direction( vec3 seed)
 {
-  /////////////////////////////////////////////////////////////////////////////
-  // Replace with your code 
-  return vec3(1,0,0);
-  /////////////////////////////////////////////////////////////////////////////
+  // https://www.scratchapixel.com/lessons/procedural-generation-virtual-worlds/perlin-noise-part-2
+  // used this source to read about uniform distribution over unit sphere
+
+  vec2 r = random2(seed);
+
+  float theta = acos(2 * r.x - 1);
+  float phi = 2 * r.y * M_PI;
+
+  return vec3(cos(phi) * sin(theta), sin(phi) * sin(theta), cos(theta));
 }
